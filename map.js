dc9bb598195881ee4f3d3742e1741aeec56718a7
Wesley <lawrencewesley40@gmail.com>
2022-05-15 11:51:02 +0300

    commited

---
+++ map.js
@@ -0,0 +1,7 @@
+// mapboxgl.accessToken = 'pk.eyJ1Ijoid2VzbGV5MjU0IiwiYSI6ImNsMzY2dnA0MDAzem0zZG8wZTFzc3B3eG8ifQ.EVg7Sg3_wpa_QO6EJjj9-g';
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/dark-v10',
+//     zoom: 17,
+//     center: [37, 1]
+// });
